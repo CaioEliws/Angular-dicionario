d5d020a8f29cbc3d13e837026e78a2cf64e37f81
CaioEliws <caioeliasvieira2@gmail.com>
2025-05-27 14:48:27 -0300

    Fix erros

--- src/app/dicionario/dicionario-routing.module.ts
+++ src/app/dicionario/dicionario-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ConsultaComponent } from './pages/consulta/consulta.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'cadastro', component: CadastroComponent },
+  { path: '', component: CadastroComponent },
   { path: 'consulta', component: ConsultaComponent },
 ];
 
--- src/app/dicionario/dicionario.module.ts
+++ src/app/dicionario/dicionario.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DicionarioRoutingModule } from './dicionario-routing.module';
-import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { ConsultaComponent } from './pages/consulta/consulta.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    HomeComponent,
     CadastroComponent,
     ConsultaComponent
   ],
