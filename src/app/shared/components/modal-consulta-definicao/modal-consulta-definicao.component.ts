af24b6c7e428db248d434a579fda5d96857fc05a
CaioEliws <caioeliasvieira2@gmail.com>
2025-05-29 12:28:21 -0300

    Complete Static Consultation Page

---
+++ src/app/shared/components/modal-consulta-definicao/modal-consulta-definicao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-consulta-definicao',
+  templateUrl: './modal-consulta-definicao.component.html',
+  styleUrls: ['./modal-consulta-definicao.component.scss']
+})
+export class ModalConsultaDefinicaoComponent {
+
+}
